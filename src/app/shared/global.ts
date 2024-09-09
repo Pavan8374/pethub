cf3fb83175e6a8f59685ce07f1fd836519cb8b4e
pavan <rpavank2000@gmail.com>
2024-09-09 18:03:26 +0530

    resolved url issue

--- src/app/shared/global.ts
+++ src/app/shared/global.ts
@@ -1,9 +1,12 @@
-import { environment } from "../../environments/environment.dev";
+import { environment } from "../../environments/environment.prod";
+//import { environment } from "../../environments/environment.dev";
 
 
 export class Global{
 
     //public static WebUrl: string = "https://freetestapi.com/api/v1/";
     //public static WebUrl: string = "https://localhost:44344/v1/";
+    //public static WebUrl: string = environment.apiUrl
     public static WebUrl: string = environment.apiUrl
+
 }
